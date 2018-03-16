81d0398b78e9685516a4efa7217eb0c71b11fc2c
shibayu36 <shibayu36@gmail.com>
2018-03-16 10:27:04 +0900

    update

--- src/main/scala/OverloadExample.scala
+++ src/main/scala/OverloadExample.scala
@@ -5,6 +5,9 @@ object OverloadExample {
 
     println(Fuga.fuga(arg => arg * 2))
     println(Fuga.fuga((arg1, arg2) => arg1 + "-" + arg2))
+
+    println(Piyo.piyo("piyo"))
+    println(Piyo.piyo(a => a * 2))
   }
 
   object Hoge {
@@ -15,5 +18,10 @@ object OverloadExample {
   object Fuga {
     def fuga(a: String => String) = a("hoge") * 3
     def fuga(a: (String, String) => String) = a("hoge", "fuga") * 3
+  }
+
+  object Piyo {
+    def piyo(a: => String): String = a * 3
+    def piyo(a: String => String): String = a("piyo") * 3
   }
 }
